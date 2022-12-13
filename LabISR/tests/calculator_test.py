49c06868a12273c251ead6067ee3e03333cfdf3f
DenisEvd <den-evdokimov-2015@mail.ru>
2022-12-14 01:46:23 +0300

    Refactor

--- LabISR/tests/calculator_test.py
+++ LabISR/tests/calculator_test.py
@@ -1,5 +1,5 @@
 import unittest
-from Calculator import Calculator
+from src.Calculator import Calculator
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
