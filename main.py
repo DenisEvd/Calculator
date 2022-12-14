f929534d0a3047546555b881afa947ced3f4af92
DenisEvd <den-evdokimov-2015@mail.ru>
2022-12-14 09:58:28 +0300

    Fix

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-class Calculator:
-    def __init__(self):
-        pass
-
-    def add(self, x1, x2):
-        return x1 + x2
-
-    def multiply(self, x1, x2):
-        return x1 * x2
-
-    def subtract(self, x1, x2):
-        return x1 - x2
-
-    def divide(self, x1, x2):
-        if x2 != 0:
-            return x1 / x2
-
-
+from Calculator import Calculator
 print ('Приветствуем вас в калькуляторе Python')
 q1 = int (input('Введите число x1: '))
 q2 = int (input('Введите число x2: '))
