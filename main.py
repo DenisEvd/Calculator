55930e1e418453b61a782ea45040fbd0432a664e
DenisEvd <den-evdokimov-2015@mail.ru>
2022-12-14 09:55:20 +0300

    Update calculator

--- main.py
+++ main.py
@@ -14,3 +14,25 @@ class Calculator:
     def divide(self, x1, x2):
         if x2 != 0:
             return x1 / x2
+
+
+print ('Приветствуем вас в калькуляторе Python')
+q1 = int (input('Введите число x1: '))
+q2 = int (input('Введите число x2: '))
+calculator = Calculator()
+
+v = int (input('Какую операцию вы хотите выполнить? \n 1 Сложение \n 2 Вычитание \n 3 Деление \n 4 Умножение \n'))
+
+if v == 1:
+    t = "сложение"
+    r = calculator.add(q1, q2)
+if v == 2:
+    r = calculator.subtract(q1, q2)
+    t = 'вычитания'
+if v == 3:
+    r = calculator.divide(q1, q2)
+    t = 'деления'
+if v == 4:
+    r = calculator.multiply(q1, q2)
+    t = 'умножения'
+print ('Результат ', t,' = ', r)
